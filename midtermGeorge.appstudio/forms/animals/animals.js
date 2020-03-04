dee08d8dfe8996bd3d06bfa1c2d09adbfad906bf
georgetmickelson <60586429+georgetmickelson@users.noreply.github.com>
2020-03-04 15:06:04 -0600

    Final

---
+++ midtermGeorge.appstudio/forms/animals/animals.js
@@ -0,0 +1,7 @@
+/*
+let myAnimals = ['dog', 'cat', 'horse', 'mouse', 'pig', 'cow', 'ferret', 'lizard', 'frog']
+
+lastAnimal = prompt('Please type a name of an animal')
+  myAnimals.push(lastAnimal)
+    alert(`The last animal is a/an ${lastAnimal}.`)
+*/
